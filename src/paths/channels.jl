bbbee59b9ae2635d9e97808d23e39db3ad69db10
Greg Peairs <gpeairs@amazon.com>
2025-10-07 14:19:22 -0700

    Test schematic version, add docs examples

--- src/paths/channels.jl
+++ src/paths/channels.jl
@@ -1,16 +1,26 @@
 """
     RouteChannel{T} <: AbstractComponent{T}
     RouteChannel(pa::Path)
+
+A channel that routes can be guided through along parallel tracks.
+
+Used in `route!` with [`Paths.SingleChannelRouting`](@ref).
+
+The `Path` used to construct a `RouteChannel` should use `Trace` styles only.
+
+A `RouteChannel` is an `AbstractComponent` with the same hooks as its `Path` and
+an empty geometry.
 """
 struct RouteChannel{T} <: AbstractComponent{T}
     path::Path{T}
     node::Node{T} # path as single node
-    capacity::Int # Currently unused
 end
+name(ch::RouteChannel) = name(ch.path)
+DeviceLayout.hooks(ch::RouteChannel) = DeviceLayout.hooks(ch.path)
 
-function RouteChannel(pa::Path{T}, capacity=0) where {T}
-    length(nodes(pa)) != 1 && return RouteChannel{T}(pa, simplify(pa), capacity)
-    return RouteChannel{T}(pa, only(nodes(pa)), capacity)
+function RouteChannel(pa::Path{T}) where {T}
+    length(nodes(pa)) != 1 && return RouteChannel{T}(pa, simplify(pa))
+    return RouteChannel{T}(pa, only(nodes(pa)))
 end
 
 # Return a node corresponding to the section of the channel that the segment actually runs through
@@ -24,14 +34,14 @@ function segment_channel_section(
 ) where {T}
     d = wireseg_stop - wireseg_start
     # Adjust for margins and track vs channel direction to get the channel node section used by actual segment
-    if abs(d) <= 2 * margin + prev_width + next_width
+    if abs(d) <= 2 * margin + prev_width / 2 + next_width / 2
         # handle case where margin consumes entire segment
         # Just have a zero length Straight at the midpoint
         track_mid = (wireseg_start + wireseg_stop) / 2
         midpoint = ch.node.seg(track_mid)
         middir = direction(ch.node.seg, track_mid)
         channel_section = Node(
-            Straight(zero(T), midpoint, middir),
+            Straight(zero(T); p0=midpoint, α0=middir),
             SimpleTrace(width(ch.node.sty, track_mid))
         )
     elseif d > zero(d) # segment is along channel direction
@@ -56,6 +66,7 @@ function segment_channel_section(
     return channel_section
 end
 
+# Actual routed path segment along a track offset from the channel path
 function track_path_segment(n_tracks, channel_section, track_idx; reversed=false)
     return offset(
         channel_section.seg,
@@ -63,6 +74,7 @@ function track_path_segment(n_tracks, channel_section, track_idx; reversed=false
     )
 end
 
+# Offset coordinate or function for the section of track with given width
 function track_section_offset(
     n_tracks,
     section_width::Coordinate,
@@ -192,33 +204,97 @@ function _reconcile_curvature!(n::Node{T}, rule::BSplineRouting) where {T}
     end
 end
 
-struct SingleChannelRouting{T} <: AbstractChannelRouting
+"""
+    struct SingleChannelRouting{T <: Coordinate} <: AbstractChannelRouting
+    SingleChannelRouting(ch::RouteChannel, transition_rule::RouteRule, margin::T)
+    SingleChannelRouting(ch::RouteChannel, transition_rules, margins)
+
+A `RouteRule` for guiding routed paths along tracks in a [`Paths.RouteChannel`](@ref).
+
+## Tracks
+
+"Tracks" are offsets of the channel's path, with equal spacing between each other
+and the extents of the channel's trace width. Tracks are ordered from right to left
+when looking along the channel. For example, track 1 is the bottom track
+(most negative offset) for a channel directed along the positive x axis, while
+the highest track index is the top track.
+
+The user manually assigns tracks to paths that will be routed with
+`rule::SingleChannelRouting` using `set_track!(rule, path)` for each path,
+prior to calling `route!(path, ...)`.
+
+If used for schematic routing, the track is supplied as a keyword argument,
+defaulting to a new track added at the top of the channel:
+`route!(g::SchematicGraph, rule, ...; track=num_tracks(rule)+1)`.
+
+## Routing
+
+A path routed from `p0` to `p1` using this rule will enter the channel
+at the channel's closest point to `p0` and exit at the closest point to `p1` if
+`margin` is zero. For nonzero `margin`, the entry and exit points are each shifted
+towards the other along the channel by `margin`, allowing more space for the
+transitions into and out of the channel.
+
+The middle "tracked" section is offset from the channel's center line according to
+the path's track, the maximum track assigned to any path by the rule,
+and the channel width.
+
+The path is routed from `p0` to the tracked section and from the tracked section
+to `p1` using `transition_rule`.
+
+Transition rules and margins can also be supplied as tuples to the constructor
+to allow different parameters for entry and exit transitions.
+"""
+mutable struct SingleChannelRouting{T <: Coordinate} <: AbstractChannelRouting
     channel::RouteChannel{T}
     transition_rules::Tuple{<:RouteRule, <:RouteRule}
     transition_margins::Tuple{T, T}
-    segment_tracks::Dict{Path{T}, Int}
+    segment_tracks::Dict{Path, Int}
+    global_channel::RouteChannel{T}
+    function SingleChannelRouting(
+        ch::RouteChannel{T},
+        rules,
+        margins,
+        tracks=Dict{Path, Int}()
+    ) where {T}
+        return new{T}(ch, rules, margins, tracks)
+    end
 end
-function SingleChannelRouting(ch::RouteChannel{T}, rule::RouteRule, margin::T) where {T}
-    return SingleChannelRouting{T}(ch, (rule, rule), (margin, margin), Dict{Path{T}, Int}())
+function SingleChannelRouting(
+    ch::RouteChannel{T},
+    rule::RouteRule,
+    margin,
+    tracks...
+) where {T}
+    return SingleChannelRouting(ch, (rule, rule), (margin, margin), tracks...)
+end
+function channel(rule::SingleChannelRouting)
+    isdefined(rule, :global_channel) && return rule.global_channel
+    return rule.channel
 end
 entry_rules(scr::SingleChannelRouting) = [first(scr.transition_rules)]
 exit_rule(scr::SingleChannelRouting) = last(scr.transition_rules)
 entry_margin(scr::SingleChannelRouting) = first(scr.transition_margins)
 exit_margin(scr::SingleChannelRouting) = last(scr.transition_margins)
-num_tracks(scr::SingleChannelRouting) = maximum(values(scr.segment_tracks))
-track_idx(scr, pa) = scr.segment_tracks[pa]
+function num_tracks(scr::SingleChannelRouting)
+    isempty(scr.segment_tracks) && return 0
+    return maximum(values(scr.segment_tracks))
+end
+function track_idx(scr, pa)
+    return scr.segment_tracks[pa]
+end
 function set_track!(scr, pa, track_idx)
     return scr.segment_tracks[pa] = track_idx
 end
 
 function track_path_segments(rule::SingleChannelRouting, pa::Path, endpt)
-    wireseg_start = pathlength_nearest(rule.channel.node.seg, p1(pa))
-    wireseg_stop = pathlength_nearest(rule.channel.node.seg, endpt)
+    wireseg_start = pathlength_nearest(channel(rule).node.seg, p1(pa))
+    wireseg_stop = pathlength_nearest(channel(rule).node.seg, endpt)
     return [
         track_path_segment(
             num_tracks(rule),
             segment_channel_section(
-                rule.channel,
+                channel(rule),
                 wireseg_start,
                 wireseg_stop,
                 2 * entry_margin(rule),
@@ -228,14 +304,4 @@ function track_path_segments(rule::SingleChannelRouting, pa::Path, endpt)
             reversed=wireseg_start > wireseg_stop
         )
     ]
-end
-
-function _update_with_graph!(
-    rule::SingleChannelRouting,
-    route_node,
-    graph;
-    track=num_tracks(rule) + 1,
-    kwargs...
-)
-    return set_track!(rule, route_node.component._path, track)
 end
