ba3773e9e6804b581732523173537e163366ae95
Greg Peairs <gpeairs@amazon.com>
2026-01-12 14:57:03 +0100

    Fix and test ambiguity

--- src/render/periodic.jl
+++ src/render/periodic.jl
@@ -7,3 +7,17 @@ function to_polygons(seg::Paths.Segment{T}, sty::Paths.PeriodicStyle; kwargs...)
         init=Polygon{T}[]
     )
 end
+
+function to_polygons(
+    seg::DeviceLayout.Paths.CompoundSegment{T},
+    sty::DeviceLayout.Paths.PeriodicStyle;
+    kwargs...
+) where {T}
+    subsegs, substys = Paths.resolve_periodic(seg, sty)
+
+    return reduce(
+        vcat,
+        to_polygons.(subsegs, undecorated.(substys); kwargs...),
+        init=Polygon{T}[]
+    )
+end
