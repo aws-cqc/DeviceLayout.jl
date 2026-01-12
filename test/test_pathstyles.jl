2492b256023d02218b2f88efd931577101d658ad
Greg Peairs <gpeairs@amazon.com>
2026-01-12 13:30:17 +0100

    Run formatter

--- test/test_pathstyles.jl
+++ test/test_pathstyles.jl
@@ -14,7 +14,7 @@
     @test psty(46μm) === (sty2, 1.0μm)
     @test Paths.extent(psty, 0μm) == 11μm
     @test Paths.width(psty, 18μm) == 2μm
-    
+
     # Various combinations work
     # Nested
     psty_nested = PeriodicStyle([sty1, psty, sty2], 50μm; weights=[1, 3, 1])
@@ -25,7 +25,7 @@
     # General, Taper, NoRender, Termination
     pa = Path(0nm, 0nm)
     straight!(pa, 4μm, Paths.CPW(x -> 10μm, x -> 6μm))
-    turn!(pa, 90°, 10μm/(pi/4), Paths.TaperCPW(10μm, 6μm, 2μm, 1μm))
+    turn!(pa, 90°, 10μm / (pi / 4), Paths.TaperCPW(10μm, 6μm, 2μm, 1μm))
     terminate!(pa; initial=true, rounding=3μm)
     terminate!(pa; rounding=0.5μm, gap=0μm)
     straight!(pa, 10μm, Paths.NoRender())
@@ -42,7 +42,7 @@
     # Termination, CPW straight, turn, termination; NoRender, Trace, Taper, Trace
     @test psty_complex.lengths ≈ [9μm, 1μm, 19.5μm, 0.5μm, 10μm, 10μm, 10μm, 10μm]
     pa2 = Path(0nm, 0nm)
-    straight!(pa2, 9*70μm + 64μm, psty_complex) # Stop just before attachment in last segment
+    straight!(pa2, 9 * 70μm + 64μm, psty_complex) # Stop just before attachment in last segment
     straight!(pa2, 2μm)
     c = Cell("test")
     render!(c, pa2, GDSMeta(1)) # Runs without error
@@ -51,4 +51,3 @@
     # Note: Attachment will be duplicated if it's at the exact end and start of a segment!
     @test split(pa2[1], 100μm)[2].sty.l0 == 100μm
 end
-
